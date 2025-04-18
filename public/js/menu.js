8345a799f57f65f0ab67a8f3fd6f2679b911fce0
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-04-18 13:51:25 +0200

    nav style

--- public/js/menu.js
+++ public/js/menu.js
@@ -14,11 +14,14 @@ export function hideMenu(btn) {
 
 export function sticky() {
     const nav = document.querySelector("nav")
+    const scrollTop = document.querySelector(".logo")
+    scrollTop.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
+
     const navTop = nav.offsetTop
 
-    function fixnavbar() {
-        console.log(window.scrollY);
-        
+    function fixnavbar() { 
         if (window.scrollY > navTop) {
             // document.body.style.paddingTop = nav.offsetHeight + "px";
             nav.classList.add("fixed-nav");
@@ -28,9 +31,6 @@ export function sticky() {
     }
 
     window.addEventListener("scroll", fixnavbar);
-
-
-
 }
 
 
